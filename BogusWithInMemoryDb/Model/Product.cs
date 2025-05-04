2e12e65f8dbef213652035d4ca6026ec60e5b56b
Bekzod Allaev <Allaev_@outlook.com>
2025-05-04 14:23:37 +0200

    Add project files.

---
+++ BogusWithInMemoryDb/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BogusWithInMemoryDb.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        [JsonIgnore]
+        public Category? Category { get; set; }
+    }
+}
