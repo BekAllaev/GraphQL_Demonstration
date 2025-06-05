8a60acf68e6700bee9196797de00a171baa1b28f
Bekzod Allaev <Allaev_@outlook.com>
2025-06-05 21:23:28 +0200

    Add CountryStatisticalObject, CustomerStatisticalObject, EmployeeStatisticalObject and SummaryStatisticalObject

---
+++ GraphQL_CQRS/StatisticalObjects/CustomerStatisticalObject.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQL_CQRS.StatisticalObjects
+{
+    public class CustomerStatisticalObject
+    {
+        public string CustomerName { get; set; }
+
+        public double Purchases { get; set; }
+    }
+}
---
+++ GraphQL_CQRS/StatisticalObjects/SummaryStatisticalObject.cs
@@ -0,0 +1,15 @@
+﻿namespace GraphQL_CQRS.StatisticalObjects
+{
+    public class SummaryStatisticalObject
+    {
+        public int AmountOfOrders { get; set; }
+
+        public double OverallSales { get; set; }
+
+        public double MaxCheck { get; set; }
+
+        public double MinCheck { get; set; }
+
+        public double AverageCheck { get; set; }
+    }
+}
